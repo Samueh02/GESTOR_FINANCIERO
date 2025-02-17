b1db875a5b10737288ece9af8295db83a5c32d59
Samuel Ortega <samujedi@gmail.com>
2025-02-17 15:36:45 +0100

    proyecto configurado

---
+++ gestor-presupuesto/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex justify-center items-center">
+      <h1 className="text-white text-3xl font-bold">¡Tailwind está funcionando! 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
+
